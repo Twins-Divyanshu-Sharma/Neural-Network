b595476c7ca0d27dc3d3c917a11c97ab8ce8da8b
Diwakar Sharma <Twins-Diwakar-Sharma@github.com>
2021-08-24 19:14:18 +0530

    Completed Quest: Layer

--- incsrc/NeuralNetwork.h
+++ incsrc/NeuralNetwork.h
@@ -1,7 +1,17 @@
 #ifndef HH_NEURAL_NETWORK_HH
 #define HH_NEURAL_NETWORK_HH
 
+#include <math.h>
+#include <random>
 #include "Algebra.h"
+
+#define EXP 2.17128128
+
+float sigmoid(float);
+float diffSigmoidOut(float);
+
+void initNormal(Mat& m);
+
 
 class Layer
 {
@@ -9,7 +19,11 @@ private:
     Vec out, dout;
     Mat m, dm;
 public:
-    Layer(int i, int o);
+    Layer(void(*initialize)(Mat& m),int i, int o);
+    ~Layer();
+    void forwardPass(float(*act)(float),Vec& in);
+    void backwardPass(float(*diffActOut)(float),Vec& in, Vec& din);
+    void halfBackwardPass(float(*diffActOut)(float),Vec& in);
 };
 
 
