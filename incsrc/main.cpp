5ee57f844d4a00cc4f77eea6616e2c4d0c937e39
Divyanshu Sharma <twins-divyanshu-sharma>
2022-01-18 19:17:42 +0530

    Corrected main.cpp

--- incsrc/main.cpp
+++ incsrc/main.cpp
@@ -1,10 +1,3 @@
-
-
-
-
-
-
-
 #include "NeuralNetwork.h"
 #include <fstream>
 #include <string>
@@ -13,22 +6,17 @@
 int main()
 {
 
-  int epoch = 1;
+  int epoch = 2;
 
   int inputSize = 28*28;
-  int h0Size = 50;
-  int h1Size = 20;
   int outputSize = 10;
-
-  float (*act)(float) = sigmoid;
-  float (*diffActOut)(float) = diffSigmoidOut;
 
   FNN fnn(inputSize); // construct
   fnn.setMatrixRandomFunc(initNormal); // set normal function
 
-  fnn = fnn + h0Size + h1Size + outputSize;
+  fnn = fnn + 50 + 20 + outputSize;
     
-  fnn.train(2,"res/trainImages", "res/trainLabels", 1); 
+  fnn.train(epoch,"res/trainImages", "res/trainLabels", 1); 
   fnn.test("res/trainImages","res/trainLabels");
 
   return 0; 
